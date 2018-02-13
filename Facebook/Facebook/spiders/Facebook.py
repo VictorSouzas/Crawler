e8f452454bfe43c09c034946713a6a5a719108c2
victor <victor_souzas@yahoo.com.br>
2018-02-13 01:21:39 -0200

    Oppening facebook Login Page

--- Facebook/Facebook/spiders/Facebook.py
+++ Facebook/Facebook/spiders/Facebook.py
@@ -2,26 +2,39 @@
 
 import scrapy
 from scrapy.http import Request
+from scrapy.http import TextResponse
+from scrapy.selector import HtmlXPathSelector
+from scrapy.selector import Selector
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from scrapy.selector import Selector
-import time
 
 
 class Facebook(scrapy.Spider):
     name = "facebook"
 
+    def __init__(self):
+        scrapy.Spider.__init__(self)
+        self.binary = FirefoxBinary('/usr/lib/firefox/firefox')
+
+        self.url = "http://facebook.com"
+
+    def login(self, response):
+        self.driver = webdriver.Firefox(firefox_binary=self.binary)
+        self.driver.get(response.url)
+        response = TextResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
+        yield {"text": response.selector.xpath('//form')}
+
     def start_requests(self):
-        url = 'http://facebook.com'
         method = 'GET'
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
-        yield Request(url, callback=self.parse, method=method, headers=headers)
+        yield Request(url=self.url, callback=self.login, method=method, headers=headers)
 
     def parse(self, response):
         # yield {'text': content.xpath('//form[@id="login_form"]').extract_first() }
-        binary = FirefoxBinary('/usr/lib/firefox/firefox')
-        self.driver = webdriver.Firefox(firefox_binary=binary)
-        self.driver.get(response.url)
-        time.sleep(10)
-        content = Selector(text=self.driver.page_source)
-        yield { 'Html': content.xpath('//form[@id="login_form"]').xpath('//table').extract() }
+        # binary = FirefoxBinary('/usr/lib/firefox/firefox')
+        # self.driver = webdriver.Firefox(firefox_binary=binary)
+        # self.driver.get(response.url)
+        # time.sleep(10)
+        # content = Selector(text=self.driver.page_source)
+        # yield {'Html': content.xpath('//form[@id="login_form"]').xpath('//table').extract()}
+        pass
